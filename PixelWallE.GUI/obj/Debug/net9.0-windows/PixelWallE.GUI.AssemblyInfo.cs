411be81c91762d3706f128689c6fc1b56756bcae
Mauricio <mackandal337@gmail.com>
2025-06-23 20:59:26 -0400

    PintaBonito

--- PixelWallE.GUI/obj/Debug/net9.0-windows/PixelWallE.GUI.AssemblyInfo.cs
+++ PixelWallE.GUI/obj/Debug/net9.0-windows/PixelWallE.GUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PixelWallE.GUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1deac62f21478a5a1d4232f4844b1943e4c2fe3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4fd34a85451a0ca2e712d50d620c3c7e8e5316c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PixelWallE.GUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PixelWallE.GUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
